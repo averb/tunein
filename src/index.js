e863dd7c9c5345382819cd1411bd7efe8210266e
Andrew <a.verbenetz@gmail.com>
2020-12-24 17:24:26 +0200

    feat(core): add basic architecture

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Stations from './stations';
+import './reset.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Stations />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/stations/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css'
+
+const Stations = () => (
+  <div className="stations">
+    Station
+  </div>
+)
+
+export default Stations
